bcb0b9b74e07230370a15ca7a411db51ef0adb9e
ABHIRAMI P S <43600476+AbhiRami007@users.noreply.github.com>
2022-12-25 19:41:01 +0530

    commented

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -1,92 +1,92 @@
-import {Request, Response} from 'express';
-import responseHelper from '../utils/responseHelper';
-import {StatusCodes} from 'http-status-codes';
-import jobs from '../services/jobs';
+// import {Request, Response} from 'express';
+// import responseHelper from '../utils/responseHelper';
+// import {StatusCodes} from 'http-status-codes';
+// import jobs from '../services/jobs';
 
-const createJobs = async (req: Request, res: Response) => {
-  try {
-    const jobInfo = await jobs.create(req.body);
-    if (jobInfo) {
-        return responseHelper.successResponse(res, StatusCodes.OK)("Job created Successfully", jobInfo);
-    }
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+// const createJobs = async (req: Request, res: Response) => {
+//   try {
+//     const jobInfo = await jobs.create(req.body);
+//     if (jobInfo) {
+//         return responseHelper.successResponse(res, StatusCodes.OK)("Job created Successfully", jobInfo);
+//     }
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const getJobsByLocationOrTitle = async (req: Request, res: Response) => {
-  try {
-    const jobInfo = await jobs.get(req.query.search, req.query.location);
-    if (jobInfo) {
-        return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
-    }
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+// const getJobsByLocationOrTitle = async (req: Request, res: Response) => {
+//   try {
+//     const jobInfo = await jobs.get(req.query.search, req.query.location);
+//     if (jobInfo) {
+//         return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
+//     }
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const getJobsByTitle = async (req: Request, res: Response) => {
-  try {
-    const jobInfo = await jobs.getByTitle(req.query.search);
-    if (jobInfo) {
-        return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
-    }
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+// const getJobsByTitle = async (req: Request, res: Response) => {
+//   try {
+//     const jobInfo = await jobs.getByTitle(req.query.search);
+//     if (jobInfo) {
+//         return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
+//     }
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const getJobsById = async (req: Request, res: Response) => {
-  try {
-    const jobInfo = await jobs.getByTitle(req.params.id);
-    if (jobInfo) {
-        return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
-    }
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+// const getJobsById = async (req: Request, res: Response) => {
+//   try {
+//     const jobInfo = await jobs.getByTitle(req.params.id);
+//     if (jobInfo) {
+//         return responseHelper.successResponse(res, StatusCodes.OK)("Job fetched Successfully", jobInfo);
+//     }
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const listJobs = async (_req: Request, res: Response) => {
-    try {   
-      const jobsInfo = await jobs.list()
-    return responseHelper.successResponse(res, StatusCodes.OK)("Job Details fetched Successfully", jobsInfo);
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+// const listJobs = async (_req: Request, res: Response) => {
+//     try {   
+//       const jobsInfo = await jobs.list()
+//     return responseHelper.successResponse(res, StatusCodes.OK)("Job Details fetched Successfully", jobsInfo);
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const updateJob = async (req: Request, res: Response) => {
-    try {   
-        const jobsInfo = await jobs.getById(req.params.id)
-        if (jobsInfo) {
-            const updated = await jobs.update(req.params.id, req.body);
-    return responseHelper.successResponse(res, StatusCodes.OK)("Job Details updated Successfully", updated);
+// const updateJob = async (req: Request, res: Response) => {
+//     try {   
+//         const jobsInfo = await jobs.getById(req.params.id)
+//         if (jobsInfo) {
+//             const updated = await jobs.update(req.params.id, req.body);
+//     return responseHelper.successResponse(res, StatusCodes.OK)("Job Details updated Successfully", updated);
             
-        }
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+//         }
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
-const removeJob = async (req: Request, res: Response) => {
-    try {   
-        await jobs.remove(req.params.id)
-    return responseHelper.successResponse(res, StatusCodes.OK)("Job Details removed Successfully");
+// const removeJob = async (req: Request, res: Response) => {
+//     try {   
+//         await jobs.remove(req.params.id)
+//     return responseHelper.successResponse(res, StatusCodes.OK)("Job Details removed Successfully");
         
-  } catch (error) {
-    responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
-  }
-};
+//   } catch (error) {
+//     responseHelper.errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR)(error.errors[0].message);
+//   }
+// };
 
 
 
-export default {
-    createJobs,
-    listJobs,
-    getJobsByTitle,
-    getJobsByLocationOrTitle,
-    getJobsById,
-    updateJob,
-    removeJob
-};
+// export default {
+//     createJobs,
+//     listJobs,
+//     getJobsByTitle,
+//     getJobsByLocationOrTitle,
+//     getJobsById,
+//     updateJob,
+//     removeJob
+// };
--- src/services/jobs.ts
+++ src/services/jobs.ts
@@ -1,85 +1,85 @@
-import sequelize from 'sequelize';
-import DB from '../models/index';
-const Op = sequelize.Op;
+// import sequelize from 'sequelize';
+// import DB from '../models/index';
+// const Op = sequelize.Op;
 
-const create = async (params) => {
-  return await DB.Jobs.create({
-    ...params,
-  });
-};
+// const create = async (params) => {
+//   // return await DB.Jobs.create({
+//   //   ...params,
+//   // });
+// };
 
-const get = async (searchTerm, location) => {
-    return DB.Jobs.findAll({
-        where: {
-            [Op.and]: {
-        location: {
-            [Op.eq]: location,
-        },
-        title: {
-            [Op.eq]: searchTerm,
-        },
+// const get = async (searchTerm, location) => {
+//   //   return DB.Jobs.findAll({
+//   //       where: {
+//   //           [Op.and]: {
+//   //       location: {
+//   //           [Op.eq]: location,
+//   //       },
+//   //       title: {
+//   //           [Op.eq]: searchTerm,
+//   //       },
                 
-    }
-    },
-  });
-};
+//   //   }
+//   //   },
+//   // });
+// };
 
-const getByTitle = async (title) => {
-    return DB.Jobs.findAll({
-        where: {
-          title
-      }
-  });
-};
+// const getByTitle = async (title) => {
+//     return DB.Jobs.findAll({
+//         where: {
+//           title
+//       }
+//   });
+// };
 
-const getById = async (id) => {
-  return DB.Jobs.findOne({
-    where: {
-      id: {
-        [Op.eq]: id,
-      },
-    },
-  });
-};
+// const getById = async (id) => {
+//   return DB.Jobs.findOne({
+//     where: {
+//       id: {
+//         [Op.eq]: id,
+//       },
+//     },
+//   });
+// };
 
-const list = async () => {
-  return DB.Jobs.findAndCountAll({
-    where: {
-      isDelete: false,
-    },
-  });
-};
+// const list = async () => {
+//   return DB.Jobs.findAndCountAll({
+//     where: {
+//       isDelete: false,
+//     },
+//   });
+// };
 
 
-const update = async (id,job) => {
-  return DB.Jobs.update(job, {
-    where: {
-      id: {
-        [Op.eq]: id,
-      },
-    },
-  });
-};
+// const update = async (id,job) => {
+//   return DB.Jobs.update(job, {
+//     where: {
+//       id: {
+//         [Op.eq]: id,
+//       },
+//     },
+//   });
+// };
 
-const remove = async (id: any) => {
-  return DB.Jobs.update(
-    {isDelete: true, isActive: false},
-    {
-      where: {
-        id: {
-          [Op.eq]: id,
-        },
-      },
-    },
-  );
-};
+// const remove = async (id: any) => {
+//   return DB.Jobs.update(
+//     {isDelete: true, isActive: false},
+//     {
+//       where: {
+//         id: {
+//           [Op.eq]: id,
+//         },
+//       },
+//     },
+//   );
+// };
 
-export default {
-  create,
-  get,
-  remove,
-  update,
-    getById,
-    getByTitle,
-  list
-};
+// export default {
+//   create,
+//   get,
+//   remove,
+//   update,
+//     getById,
+//     getByTitle,
+//   list
+// };

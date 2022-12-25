d265deee3d8f28b1ba35851484b1417f245d9b24
ABHIRAMI P S <43600476+AbhiRami007@users.noreply.github.com>
2022-12-25 15:33:06 +0530

    Update sequelize.js

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -9,7 +9,7 @@ module.exports = {
         password: process.env.DB_PASS,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        dialect: 'postgres',
         dialectOptions: {
             ssl: {
                 require: true,
@@ -29,7 +29,7 @@ module.exports = {
         password: process.env.DB_PASS,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        dialect: 'postgres',
         dialectOptions: {
             ssl: {
                 require: true,
